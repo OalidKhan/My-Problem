a9b9b1e209c47377f9e253fd660199ba24ae239b
OalidKhan <oalidkhan69@gmail.com>
2025-08-19 20:11:33 +0600

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    long long int num,i,factorial=1;
+    printf("Enter any positive number: ");
+    scanf("%lld",&num);
+    for ( i=1 ; i<num ; i++ )
+    {
+        factorial = factorial * i;
+        printf("%lld * ",i);
+    }
+    factorial = factorial * num;
+    printf("%lld = %lld",num,factorial);
+
+    return 0;
+}
