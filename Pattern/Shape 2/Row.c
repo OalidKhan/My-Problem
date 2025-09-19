85911b50f6fee85d6f24fc04618c2440730b8f67
Oalid Khan <oalidkhan69@gmail.com>
2025-09-20 00:06:32 +0600

    Create Row.c

---
+++ Pattern/Shape 2/Row.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    printf("Enter N = ");
+    scanf("%d",&N);
+    for (int r=1 ; r<=N ; r++)
+    {
+        for (int c=1 ; c<=r ; c++)
+        {
+            printf("%d ",r);
+        }
+        printf("\n");
+    }
+    for (int r=N-1 ; r>=1 ; r--)
+    {
+        for (int c=1 ; c<=r ; c++)
+        {
+            printf("%d ",r);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
