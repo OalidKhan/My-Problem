7f622ff1d4bac54e600fdeb285d80cfd5e2ba168
Oalid Khan <oalidkhan69@gmail.com>
2025-09-20 00:13:21 +0600

    Create Star.c

---
+++ Pattern/Shape 3/Left Triangle/Star.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    printf("Enter N = ");
+    scanf("%d",&N);
+    for (int r=1 ; r<=N ; r++)
+    {
+        for (int s=r ; s<N ; s++)
+        {
+            printf(" ");
+        }
+        for (int c=1 ; c<=r ; c++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
