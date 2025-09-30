688263815050947e0665dd9d78499a2b187630c9
Oalid Khan <oalidkhan69@gmail.com>
2025-09-30 15:28:40 +0600

    Create Max.c

---
+++ Array/Find Max With Array/Max.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,s;
+    int Numbers[s];
+    int Max = Numbers[0];
+    int Min = Numbers[0];
+    scanf("%d",&i);
+    for(s=0 ; s<i ; s++)
+    {
+        scanf("%d",&Numbers[s]);
+    }
+    for (int index=1 ; index<s ; index++)
+    {
+        if (Max < Numbers[index])
+        {
+            Max = Numbers[index];
+        }
+    }
+    
+    printf("The maximum number is %d\n",Max);
+    
+    return 0;
+}
